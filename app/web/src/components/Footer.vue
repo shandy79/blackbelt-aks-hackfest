7f6ad28a38d43b2ec380add418524d0bcc9bceef
Steven Handy <shandy3@jhu.edu>
2018-04-10 11:05:44 -0400

    AKS Hack CI/CD commit 4

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Vessel Global Blackbelt Forge
+        Vessel Global Blackbelt Forge and Foundry
       </div>
       <div class="col-lg-6">
       </div>
