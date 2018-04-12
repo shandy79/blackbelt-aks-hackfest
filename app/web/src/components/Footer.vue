efbbeae1364c5883a5514207e5434b7622e8f7be
Steven Handy <shandy3@jhu.edu>
2018-04-12 10:10:47 -0400

    AKS Workshop reconciliation commits

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Vessel Global Blackbelt Forge and Foundry
+        Shandy3 Global Black Belt Dojo
       </div>
       <div class="col-lg-6">
       </div>
